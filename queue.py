1814920a2a0b0499216b1127805d55c19361b6f3
Kuazk <allenz@M1-Mac-Allen.local>
2022-07-29 20:14:57 -0500

    update

---
+++ queue.py
@@ -0,0 +1,59 @@
+
+import time
+
+class Queue:
+
+    def __init__(self,size):
+
+        self.size = size
+        self.front = -1
+        self.rear = -1
+        self.arr = ['_'] * size
+
+    def isEmpty(self):
+        return self.rear == self.front
+    def isFull(self):
+        return (self.rear+1) == self.front
+    def enqueue(self,value):
+        if self.isFull():
+            return False
+        elif self.isEmpty():
+            self.rear +=1
+            self.arr[self.rear] = value
+        else: 
+            self.rear +=1
+            if self.rear == self.size:
+                self.rear = 0
+            self.arr[self.rear] = value
+        return True
+
+    def dequeue(self):
+        if self.isEmpty():
+            return -1
+        self.front += 1 #
+        if self.front == self.size:
+            self.front =0
+        x = self.arr[self.front]
+        self.arr[self.front] = '_'
+        return x
+    def Display(self):
+        
+        for i in self.arr:
+            print(f'{i}', end = ' ')
+
+queued = Queue(10)
+queued.enqueue(1)
+queued.enqueue(2)
+queued.enqueue(3)
+queued.enqueue(2)
+queued.enqueue(1)
+
+count = 1
+queued.Display()
+print('')
+while count <99:
+    count +=1 
+    queued.enqueue(queued.dequeue())
+    queued.Display()
+    print('')
+    time.sleep(0.5)
--- stack.py
+++ stack.py
@@ -1,1 +1,29 @@
-ß
+
+
+def isBalance(arr): 
+    dict = {
+        '}' : '{',
+        ')' : '(',
+        ']' : '[',   
+        }
+    stack = []
+    for i in arr:
+        if i in '{[(': 
+            stack.append(i)
+        elif i in '})]':
+            if not stack:
+                return False
+            val = stack.pop()
+            if val != dict[i]:
+                return False
+    
+    return True if not stack else False
+
+        
+
+
+
+
+str = list('{((+b)*(c+d))}]')
+
+print(isBalance(str))
